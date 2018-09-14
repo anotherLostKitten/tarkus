aa7e1721f4bca6b55633eb89aa2e98ae67a77438
Theodore Peters <tpeters@stuy.edu>
2018-09-14 00:29:13 -0400

    cleaner file reading

--- 03_occupation/weightedJobPicker.py
+++ 03_occupation/weightedJobPicker.py
@@ -6,33 +6,43 @@
 from os import getcwd
 from os.path import isfile
 from random import random
+from csv import reader
 
 # Repeatedly prompts user for a file from working directory until they select a valid file.
 #   prompt specifies prompt text.
 def getPath(prompt):
     while True:
-        path = getcwd() + "/" + input(prompt)
+        userin = input(prompt)
+        path = getcwd() + "/" + userin
+        if userin == "":
+            return path + "occupations.csv"
         if isfile(path):
             return path
         print("File not found. Try again.\n")
 
-# Formats a user chosen file to a dictionary that maps
-# strings to float values representing weights.
-# workingDict modified by function, returns sum of weigts.
-def cvsToDict(workingDict):
-    f = open(getPath("File name (occupations.csv, perhaps?) << "), 'r')
-    lines = f.readlines()
-    total = 0
-    for i in lines[1 : ]: # skipping first line, which is header
-        comma = i.rfind(",")
-        if i[ : comma] != "Total": # total percent should not be in dict
-            # vv   adds a item : percent chance key/val pair to given dict
-            workingDict[i[ : comma].replace("\"", "")] = float(i[comma + 1 : -1])
-            total += float(i[comma + 1 : -1])
-    f.close()
-    return total # return total value to make calculation of weighted random easier
+# Reads occupations.csv, returns a dictionary with occupation-string keys and weighted-floating-point values
+def csvToDict():
+    # prepare an occupations dictionary to be returned...
+    occupations = {}
 
-# Picks a random number based on the weigts given in a dict of strings to their weights.
+    # open the csv file object, bind to variable
+    csvFileObject = open(getPath("Choose file name. Leave blank for occupations.csv. | "), 'r')
+    # read the records in the csv
+    readerObject = reader(csvFileObject)
+    
+    for record in readerObject:
+        # skip over the first & last records
+        if record[0] == "Job Class" or record[0] == "Total":
+            continue
+        # record[0] is the occupation string, record[1] is the floating point (as a string)
+        # must convert appropriate string into floating point before placing as value in dict
+        else:
+            occupations[ record[0] ] = float(record[1])
+            
+    csvFileObject.close()
+    return occupations
+
+# Picks a random dict value based on the weigts given in a dict of strings to their weights.
 def weightedRandom(wc, total = None):
     if total == None: # If the total of the weights is known, can avoid calculating sum
         total = sum(wc.values()) # Possibly useful if finding weighted random several times.
@@ -41,15 +51,14 @@ def weightedRandom(wc, total = None):
     for i in wc:
         curWeight += wc[i]
         if curWeight >= endWeight:
-            return i
+            return i # dict value.
     raise ValueError("Value of total passed was incorrect.") # If the function gets here,
                                                              # user pased a total value that
                                                              # was wrong.
 
 
 
-d = dict()
-cvsToDict(d)
-print(d)
+d = csvToDict()
+print(weightedRandom(d))
 
     
