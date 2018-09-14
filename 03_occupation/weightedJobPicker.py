d48b075123173f8757f69f43eba583b83e13cb00
Theodore Peters <tpeters@stuy.edu>
2018-09-13 22:57:36 -0400

    updated for readability

--- 03_occupation/weightedJobPicker.py
+++ 03_occupation/weightedJobPicker.py
@@ -6,28 +6,33 @@
 from os import getcwd
 from os.path import isfile
 
+# Repeatedly prompts user for a file from working directory until they select a valid file.
+#   prompt specifies prompt text.
 def getPath(prompt):
     while True:
         path = getcwd() + "/" + input(prompt)
-        print(path)
         if isfile(path):
             return path
+        print "File not found. Try again.\n"
 
+# Formats a user chosen file to a dictionary that maps
+# strings to float values representing weights.
+# workingDict modified by function, returns sum of weigts.
 def cvsToDict(workingDict):
     f = open(getPath("File name (occupations.csv, perhaps?) << "), 'r')
     lines = f.readlines()
     total = 0
-    for i in lines[1 : ]:
+    for i in lines[1 : ]: # skipping first line, which is header
         comma = i.rfind(",")
-        if i[ : comma] == "Total":
-            total = float(i[comma + 1 : -1])
-        elif i[0] == "\"":
-            workingDict[i[1 : comma - 1]] = float(i[comma + 1 : -1])
-        else:
-            workingDict[i[ : comma]] = float(i[comma + 1 : -1])
+        if i[ : comma] != "Total": # total percent should not be in dict
+            # vv   adds a item : percent chance key/val pair to given dict
+            workingDict[i[ : comma]].replace("\"", "") = float(i[comma + 1 : -1])
+            total += float(i[comma + 1 : -1])
     f.close()
-    return total
+    return total # return total value to make calculation of weighted random easier
 
+
+# test code
 d = dict()
 cvsToDict(d)
 print(d)
