aed832b072cca9d1cc680293730e889dfb24f8fb
anotherLostKitten <tpeters@stuy.edu>
2018-09-22 23:49:16 -0400

    my favourite thing about editing the wrong file is... no, i can't think of anything.

--- 03_occupation/weightedJobPicker.py
+++ 03_occupation/weightedJobPicker.py
@@ -1,42 +1,59 @@
-# Theodore Peters, Jason Lin -- CuriousIncident
+# Didactic Katydids -- Theodore Peters, Raymond Wu
 # SoftDev1 pd7
-# K10 -- Jinja Tuning
-# 2018-09-24
+# K06 -- Stl/O: Divine your Destiny!
+# 2018-09-14
 
-from flask import Flask, render_template
 from os import getcwd
+# 'getcwd' method from 'os' module -- Return a string representing the current working directory. <0>
+
+from os.path import isfile
+# 'isfile' method from 'os.path' module -- Return True if [specified] path is an existing regular file. <1>
+
 from random import random
+# imports the 'random' method from the 'random' module <2>                                                                                                                                                                                                               
+# Return the next random floating point number in the range [0.0, 1.0)
+# Return a k sized list of elements chosen from the population with replacement.                                                                                                                                                                                           
+# If a weights sequence is specified, selections are made according to the relative weights.  
+
 from csv import reader
+# imports the 'reader' method from the 'csv' module to parse through csv file <3>
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Do you want to look at <a href='/occupations'>job?</a>"
+# Repeatedly prompts user for a file from working directory until they select a valid file.
+#   prompt specifies prompt text.
+def getPath(prompt, defaultPath):
+    while True:
+        userin = input(prompt)
+        path = getcwd() + "/" + userin
+        if userin == "":
+            return path + defaultPath
+        if isfile(path):
+            return path
+        print("File not found. Try again.\n")
 
-@app.route("/occupations")
-def template():
-    d = csvToDict()
-    t = round(sum(d.values()), 1)
-    r = weightedRandom(d, t)
-    return render_template("/a.html", jobDict = d, result = r, total = t)
-
+# Reads occupations.csv, returns a dictionary with occupation-string keys and weighted-floating-point values
 def csvToDict():
     # prepare an occupations dictionary to be returned...
     occupations = {}
 
     # open the csv file object, bind to variable
-    csvFileObject = open("data/occupations.csv", 'r')
+    csvFileObject = open(getPath("Choose file name. Leave blank for occupations.csv. | ", "occupations.csv"), 'r')
     # read the records in the csv
     readerObject = reader(csvFileObject)
-
+    
     for record in readerObject:
         # skip over the first & last records
-        if record[0] != "Job Class" and record[0] != "Total":
-            occupations[record[0]] = float(record[1])
+        if record[0] == "Job Class" or record[0] == "Total":
+            continue
+        # record[0] is the occupation string, record[1] is the floating point (as a string)
+        # must convert appropriate string into floating point before placing as value in dict
+        else:
+            occupations[ record[0] ] = float(record[1])
+            
     csvFileObject.close()
     return occupations
 
+# Picks a random dict value based on the weigts given in a dict of strings to their weights.
 def weightedRandom(wc, total = None):
     if total == None: # If the total of the weights is known, can avoid calculating sum
         total = sum(wc.values()) # Possibly useful if finding weighted random several times.
@@ -46,8 +63,17 @@ def weightedRandom(wc, total = None):
         curWeight += wc[i]
         if curWeight >= endWeight:
             return i # dict value.
-    raise ValueError("Value of total passed was incorrect.")
+    raise ValueError("Value of total passed was incorrect.") # If the function gets here,
+                                                             # user passed a total value that
+                                                             # was wrong.
 
-if __name__ == "__main__":
-    app.debug = True
-    app.run()
+
+dict = csvToDict()
+print( weightedRandom(dict) )
+
+    
+# References
+# <0> https://docs.python.org/3/library/os.html#os.getcwd
+# <1> https://docs.python.org/3/library/os.path.html#os.path.isfile
+# <2> https://docs.python.org/3/library/random.html#random.choices
+# <3> https://docs.python.org/3/library/csv.html#csv.reader
