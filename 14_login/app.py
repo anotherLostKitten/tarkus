eb2b2b74569a29c0637591309eff4d823d18c046
Theodore Peters <tpeters@stuy.edu>
2018-10-01 23:17:21 -0400

    merge with partners code

--- 14_login/app.py
+++ 14_login/app.py
@@ -1,28 +1,39 @@
-from flask import Flask, session, render_template, request
-from os import urandom
+#Imad Belkebir
+#SoftDev1 pd7
+#K14 -- Do I Know You?
+#2018-10-01
+
+from flask import Flask, render_template, request, session, redirect, url_for
+import os
 app = Flask(__name__)
 
-users = {"Sheldon", "bazinga"}
+user = "Fluffy"
+password = "kachow"
+app.secret_key = os.urandom(32)
 
 @app.route("/")
+def root():
+    print(session)
+    if 'username' in session.keys():
+        return render_template("welcome.html", var1=session['username'])
+    return redirect(url_for("login"))
+
+@app.route("/login", methods=["GET","POST"])
 def login():
-    getKey()
-    return "hello"
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.form.get('username') != user:
+        return render_template("login.html", var1="Error! User does not exist")
+    if request.form.get('pass') != password:
+        return render_template("login.html", var1="Error! Incorect password")
+    session['username'] = request.form.get('username')
+    return redirect(url_for("root"))
 
+@app.route("/logout")
+def logout():
+    session.pop('username')
+    return redirect(url_for("root"))
 
-def getKey():
-    print("a")
-    try:
-        p = open("data/secret.txt", "r")
-        fl = p.readlines()
-        app.secret_key = f1[0][:32]
-        p.close()
-    except FileNotFoundError:
-        p = open("data/secret.txt", "w")
-        app.secret_key = urandom(32)
-        p.write(app.secret_key)
-        p.close()
-    print(app.secret_key)
 if __name__ == "__main__":
     app.debug = True
-    app.run()
+    app.run();
