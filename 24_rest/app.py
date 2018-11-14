1e125180042022c564fa013ba3b98e771be920b1
Theodore Peters <tpeters@stuy.edu>
2018-11-13 21:13:43 -0500

    slam grandwitch

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -6,16 +6,16 @@
 from flask import Flask, render_template
 
 import urllib, json
-
 app = Flask(__name__)
 
 @app.route("/")
 def a():
-    u = urllib.request.urlopen("http://gunsandsand.com/")
+    u = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2007-04-24")
     response = u.read()
     print(response)
-    #data = json.loads(response)
-    return render_template("bazinga.html", earl="tarkus.png")
+    data = json.loads(response)
+    print(data)
+    return render_template("bazinga.html", earl=data["url"], desk=data["explanation"])
 
 if __name__ == "__main__":
     app.debug = True
