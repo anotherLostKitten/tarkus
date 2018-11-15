f65429d3029b0a9cb038b3cfbbdb0b1745fdcbe5
anotherLostKitten <tpeters@stuy.edu>
2018-11-15 13:22:55 -0500

    .asdasfkhuqegfvuioasbdgv

---
+++ 26_rrreeesssttt/app.py
@@ -0,0 +1,32 @@
+# Theodore Peters
+# SoftDev1 pd7
+# K26 -- a b c d e f g h i j k l m n o p q r s t u v w x y z
+# 2018-11-16
+
+from flask import Flask, render_template
+
+import urllib, json
+app = Flask(__name__)
+
+@app.route("/")
+def a():
+    u = urllib.request.urlopen("https://api.icndb.com/jokes/random")
+    response = u.read()
+    #print(response)
+    data = json.loads(response)
+    #print(data)
+    u.close()
+    
+    u = urllib.request.urlopen("https://dog.ceo/api/breed/Dachshund/image/random")
+    response = u.read()
+    data1 = json.loads(response)
+    
+    
+    u = urllib.request.urlopen("http://numbersapi.com/42/trivia")
+    data2 = u.read()
+    
+    return render_template("bazinga.html", desk=data["value"]["joke"], earl1=data1["message"], desk2=data2)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
