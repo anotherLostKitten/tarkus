311f864e0bdf32dbd88ec64fcace00e6364dc1bf
Theodore Peters <tpeters@stuy.edu>
2018-12-20 20:14:48 -0500

    this commit brought to you by computer interaction club gang gang

--- 30_js/dommy.js
+++ 30_js/dommy.js
@@ -3,24 +3,40 @@
 // K28 -- sequential regression III: Season of the Witch
 // 2018-12-21
 
-var thelist = Document.getElementById("thelist");
+var thelist = document.getElementById("thelist");
 var theelements = thelist.getElementsByTagName("li");
-var header = Document.getElementById("h");
+var header = document.getElementById("h");
 
 var removeItem = (e) => {
-    
+	e.remove();
 }
 
-for (var e in theelements){
+for (let i = 0; i < theelements.length; i++){
+	let e = theelements[i];
     e.addEventListener('mouseover',()=>{header.innerHTML=e.innerHTML;});
     e.addEventListener('mouseout',()=>{header.innerHTML="Hello, World!";});
     e.addEventListener('click',()=>{removeItem(e);});
 }
 
 var addItem = () => {
-    var e=document.createElement("li");
+    let e=document.createElement("li");
     e.innerHTML = "wORD";
-    thelist.appendChild(e);
+	thelist.appendChild(e);
+	e.addEventListener('mouseover',()=>{header.innerHTML=e.innerHTML;});
+    e.addEventListener('mouseout',()=>{header.innerHTML="Hello, World!";});
+    e.addEventListener('click',()=>{removeItem(e);});
 }
 
-document.getLementById("b").addEventListener('click', addItem);
+document.getElementById("b").addEventListener('click', addItem);
+
+var fiblist = document.getElementById("fiblist");
+var fib = () => {
+	let fibelements = fiblist.getElementsByTagName("li");
+	console.log(fibelements.length);
+	let n = fibelements.length<2 ? fibelements.length : parseInt(fibelements[fibelements.length-1].innerHTML) + parseInt(fibelements[fibelements.length - 2].innerHTML);
+	let e = document.createElement("li");
+	e.innerHTML = n;
+	fiblist.appendChild(e);
+}
+
+document.getElementById("fb").addEventListener("click",fib);
